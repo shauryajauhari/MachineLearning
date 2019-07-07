b15146164a35b9d68b258d71df7dfe327354d516
Shaurya Jauhari <shaurya126906@st.jmi.ac.in>
2019-07-08 00:54:39 +0800

    Add deep learning props

--- Machine_Learning_Deep_Learning/Deep_Learning.md
+++ Machine_Learning_Deep_Learning/Deep_Learning.md
@@ -1,7 +1,7 @@
 Deep Learning
 ========================================================
 author: Shaurya Jauhari, Mora Lab, GMU. (Email: shauryajauhari@gzhmu.edu.cn)
-date: 2019-07-03 11:50:49
+date: 2019-07-07 23:22:32
 autosize: true
 
 RoadMap
@@ -9,4 +9,5 @@ RoadMap
 
 - Neural Networks
   - Introduction
-  - 
+    - Artificial Intelligence
+    - Deep Learning
---
+++ Machine_Learning_Deep_Learning/keras_practice_code.R
@@ -0,0 +1,10 @@
+## Installing packages and loading libraries.
+
+install.packages("keras", type = "source", repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN/")
+library(keras)
+
+## The following function helps to setup the "TensorFlow" work environment and 
+## points any necessary dependencies to be installed. Note that the directions are
+## subjective and may vary from system to system.
+
+install_keras()
