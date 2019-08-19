58acf2de332f22f9a0e2ca3a656e70e305aca061
Shaurya Jauhari <ishauryajauhari@gmail.com>
2019-08-19 15:11:19 +0800

    Process dhs, histone mark, and chip-seq files

--- Machine_Learning_Deep_Learning/dataset_prep.R
+++ Machine_Learning_Deep_Learning/dataset_prep.R
@@ -58,3 +58,48 @@ my_consolidated_dataset$H3K27ac <- as.numeric(my_consolidated_dataset$H3K27ac)
 
 ## Creating a surrogate feature/ variable for chrom, start, and end features.
 my_consolidated_dataset$Bin <- as.character(1:nrow(my_consolidated_dataset))
+
+
+
+
+### Superimposing Data ###
+
+library(GenomicRanges)
+dhs <- read.csv("./data/41009_peaks.bed", sep = '\t', header = FALSE)
+ep300 <- read.csv("./data/41271_peaks.bed", sep = '\t', header = FALSE)
+histm <- read.csv("./data/41276_peaks.bed", sep = '\t', header = FALSE)
+
+## Selecting the first 3 columns ##
+
+dhs <- dhs[,c(1,2,3)]
+colnames(dhs) <- c("chrom","start","end")
+ep300 <- ep300[,c(1,2,3)]
+colnames(ep300) <- c("chrom","start","end")
+histm <- histm[,c(1,2,3)]
+colnames(histm) <- c("chrom","start","end")
+
+## Filtering out irrelavant/ random chromosome entries ##
+
+chromosomes <- c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14",
+                 "chr15", "chr16", "chr17", "chr18", "chr19", "chr20","chr21", "chr22", "chrX", "chrY")
+dhs <- as.data.frame(dhs[dhs$chrom %in% chromosomes, ])
+ep300 <- as.data.frame(ep300[ep300$chrom %in% chromosomes, ])
+histm <- as.data.frame(histm[histm$chrom %in% chromosomes, ])
+
+
+## Ordering on the basis of chromosomes ##
+
+dhs <- dhs[with(dhs, order(chrom, start)), ]
+ep300 <- ep300[with(ep300, order(chrom, start)), ]
+histm <- histm[with(histm, order(chrom, start)), ]
+
+## Converting the dataframes as GRanges object ##
+
+dhs <- GRanges(dhs)
+ep300 <- GRanges(ep300)
+histm <- GRanges(histm)
+
+## Finding commonalities ##
+
+in1 <- merge(dhs,ep300)
+in2 <- merge(in1,histm)
