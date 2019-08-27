62340c1b6d5a7e5a7e742f51e98c9d7b432e4911
Shaurya Jauhari <shaurya126906@st.jmi.ac.in>
2019-08-27 14:05:08 +0800

    Update notebook

--- Machine_Learning_Deep_Learning/train_data_preparation.R
+++ Machine_Learning_Deep_Learning/train_data_preparation.R
@@ -1,2 +1,4 @@
 ## Importing relevant data ##
 
+dhs <- read.csv("./data/H1_Cell_Line/GSM878621_H1_DNase.bed.gz", sep = '\t', header = FALSE)
+p300 <- read.csv("./data/H1_Cell_Line/GSM831036_H1_P300.bed.gz", sep = '\t', header = FALSE) 
