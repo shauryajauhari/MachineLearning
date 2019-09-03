0fea97e857bec9c9f53826e2ecf7ddd50c241f27
Shaurya Jauhari <ishauryajauhari@gmail.com>
2019-09-03 15:47:21 +0800

    Update code for training data

--- Machine_Learning_Deep_Learning/train_data_preparation.R
+++ Machine_Learning_Deep_Learning/train_data_preparation.R
@@ -24,14 +24,14 @@ p300 <- as.data.frame(p300[p300$chrom %in% chromosomes, ])
 
 ## Saving files
 
-write.table(p300,"./data/H1_Cell_Line/h1_p300.bed",sep="\t",row.names=FALSE, quote = FALSE)
-write.table(dhs,"./data/H1_Cell_Line/h1_dhs.bed",sep="\t",row.names=FALSE, quote = FALSE)
+write.table(p300,"./data/H1_Cell_Line/h1_p300.bed", sep="\t", row.names=FALSE, quote = FALSE)
+write.table(dhs,"./data/H1_Cell_Line/h1_dhs.bed", sep="\t", row.names=FALSE, quote = FALSE)
 
 ## Add class to the data: "enhancer"
 
 h1_p300_dhs_intersect <- read.table("./data/H1_Cell_Line/h1_p300_dhs_intersect.bed", sep = "\t")
 h1_p300_dhs_intersect$V4 <- "enhancer"
-write.table(dhs,"./data/H1_Cell_Line/h1_p300_dhs_intersect_class.bed",sep="\t",row.names=FALSE, quote = FALSE)
+write.table(h1_p300_dhs_intersect,"./data/H1_Cell_Line/h1_p300_dhs_intersect_class.bed",sep="\t",row.names=FALSE, quote = FALSE)
 
 #################################################################################################################
 
@@ -54,17 +54,42 @@ tss_sites_test$Transcript.end..bp. <- tss_sites_test$Transcript.end..bp.+3000
 
 ## Sourcing chromosomal lengths in the human genome for generating random tracks
 
-hg19_chrom_sizes <- read.csv(url("https://genome.ucsc.edu/goldenPath/help/hg19.chrom.sizes"), sep = "\t", header = FALSE, col.names = c("chrom", "size"))
+hg19_chrom_sizes <- read.table(url("https://genome.ucsc.edu/goldenPath/help/hg19.chrom.sizes"), sep = "\t", header = FALSE, col.names = c("chrom", "size"))
 hg19_chrom_sizes <- as.data.frame(hg19_chrom_sizes[hg19_chrom_sizes$chrom %in% chromosomes, ])
 
 ## Saving file for generating random tracks via 'bedtools random' function ##
-write.table(hg19_chrom_sizes,"./data/H1_Cell_Line/hg19.genome",sep="\t",row.names=FALSE, quote = FALSE)
+write.table(hg19_chrom_sizes,"./data/H1_Cell_Line/hg19.genome", sep="\t", row.names=FALSE, quote = FALSE)
 
 ## Recalling
 
-hg19_random_tracks <- read.table("./data/H1_Cell_Line/hg19_random_tracks_sorted.bed", sep = "\t", header = FALSE)
+hg19_random_tracks <- read.table("./data/H1_Cell_Line/hg19_random_tracks.bed", sep = "\t", header = FALSE)
 hg19_random_tracks <- hg19_random_tracks[,c(1,2,3)]
+hg19_random_tracks_ordered <- hg19_random_tracks[order(hg19_random_tracks[,1],hg19_random_tracks[,2]),]
+
+write.table(hg19_random_tracks_ordered,"./data/H1_Cell_Line/hg19_random_tracks_sorted_required.bed", sep="\t", row.names=FALSE, quote = FALSE)
 
 ## Choosing random sites distal to TSS
 
+## The strategy is to combine random tracks distal to the TSS or p300 binding sites. Now, let us create a 
+## combination of TSS and p300 binding sites and then subtract the random sites from these, thus giving us
+## the residuals.
 
+tss_final <- read.table("./data/H1_Cell_Line/tss_sites_header_removed.bed", sep = "\t", header = FALSE)
+p300_final <- read.table("./data/H1_Cell_Line/h1_p300_merged.bed", sep = "\t", header = FALSE)
+
+## Combine the intervals and not 'merge' them
+tss_or_p300 <- rbind(tss_final,p300_final)
+
+## sort on the basis of chromosome names.
+tss_or_p300 <- tss_or_p300[order(tss_or_p300[,1]),]
+
+## Output file.
+write.table(tss_or_p300,"./data/H1_Cell_Line/tss_or_p300.bed", sep="\t", row.names=FALSE, quote = FALSE)
+
+
+## Import resultant files from intersection.
+negative_class <- read.table("./data/H1_Cell_Line/negative_class.bed", sep = "\t", header = FALSE)
+negative_class$V4 <- "Non-Enhancer"
+write.table(negative_class,"./data/H1_Cell_Line/negative_class.bed", sep="\t", row.names=FALSE, quote = FALSE)
+
+positive_class <- read.table("./data/H1_Cell_Line/h1_p300_dhs_intersect_class_header_removed.bed", sep = "\t", header = FALSE)
