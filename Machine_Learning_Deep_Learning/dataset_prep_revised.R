2cb288727ce6f3162a3681660e022984edf87e6e
Shaurya Jauhari <ishauryajauhari@gmail.com>
2019-12-16 15:25:32 +0800

    Add props, R data objects, code update

--- Machine_Learning_Deep_Learning/dataset_prep_revised.R
+++ Machine_Learning_Deep_Learning/dataset_prep_revised.R
@@ -23,13 +23,10 @@ h3k4me1_gr <- as(h3k4me1,"GRanges")
 ## Importing the merged BEDGRAPH/BW file
 bedtools_unionbedg_all.bw <- system ("bedtools unionbedg -i ./data/H1_Cell_Line/H3K27ac/ENCFF663SAM2000_30.bw ./data/H1_Cell_Line/H3K4me3/ENCFF340UJK2000_36.bw ./data/H1_Cell_Line/H3K4me2/ENCFF799BDH2000_36.bw ./data/H1_Cell_Line/H3K4me1/ENCFF441KOL2000_36.bw -header -names H3K27ac H3K4me3 H3K4me2 H3K4me1")
 merged_bw <- read.csv("bedtools_unionbedg_all.bw", sep = '\t', header = TRUE)
-#merged_bw <- merged_bw[-1,]
-head(merged_bw)
 merged_bw$class <- "enhancer"
 head(merged_bw)
 
 ## Identifying examples of standard chromosomes only and filtering the residuals.
-
 chromosomes <- c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14",
                  "chr15", "chr16", "chr17", "chr18", "chr19", "chr20","chr21", "chr22", "chrX", "chrY")
 merged_bw<- as.data.frame(merged_bw[merged_bw$chrom %in% chromosomes, ])
@@ -57,64 +54,63 @@ input_score_data <- merged_bw
 input_score_data<- as.data.frame(input_score_data[input_score_data$chrom %in% chromosomes, ])
 
 
-## Replacing NAs with 0s(zeros) in the peaks' columns. Since the score data is not available, imputing empty cells with
+## Replacing NAs with 0s(zeros) in the reads' columns. Since the score data is not available, imputing empty cells with
 ## zero entries engenders mathematical convenience.
 
-input_score_data$V4[is.na(input_score_data$V4)] <- 0 
-input_score_data$V5[is.na(input_score_data$V5)] <- 0 
-input_score_data$V6[is.na(input_score_data$V6)] <- 0 
-input_score_data$V7[is.na(input_score_data$V7)] <- 0 
+input_score_data$H3K27ac[is.na(input_score_data$H3K27ac)] <- 0 
+input_score_data$H3K4me3[is.na(input_score_data$H3K4me3)] <- 0 
+input_score_data$H3K4me2[is.na(input_score_data$H3K4me2)] <- 0 
+input_score_data$H3K4me1[is.na(input_score_data$H3K4me1)] <- 0 
 
 ## Converting data to GRanges objects
-library(GenomicRanges)
+positive_class <- positive_class[-1,]
 positive_class_labels <- GRanges(seqnames = positive_class$V1, ranges = IRanges(start = positive_class$V2, 
                                                                        end = positive_class$V3))
-mcols(positive_class_labels) <- DataFrame(class= "Enhancer")
+mcols(positive_class_labels) <- DataFrame(class= "enhancer")
 
 
 negative_class_labels <- GRanges(seqnames = negative_class$V1, ranges = IRanges(start = negative_class$V2, 
                                                                        end = negative_class$V3))
-mcols(negative_class_labels) <- DataFrame(class= "Non-Enhancer")
+mcols(negative_class_labels) <- DataFrame(class= "non-enhancer")
 
 
-input_score <- GRanges(seqnames = input_score_data$V1, ranges = IRanges(start = input_score_data$V2,
-                                                                        end = input_score_data$V3))
-mcols(input_score) <- DataFrame(peaks_h3k27ac = input_score_data$V4, peaks_h3k4me3 = input_score_data$V5,
-                                peaks_h3k4me2 = input_score_data$V6, peaks_h3k4me1 = input_score_data$V7)
+input_score <- GRanges(seqnames = input_score_data$chrom, ranges = IRanges(start = input_score_data$start,
+                                                                        end = input_score_data$end))
+mcols(input_score) <- DataFrame(reads_h3k27ac = input_score_data$H3K27ac, reads_h3k4me3 = input_score_data$H3K4me3,
+                                reads_h3k4me2 = input_score_data$H3K4me2, reads_h3k4me1 = input_score_data$H3K4me1)
 
 ## Performing merge to figure out the score and class matrix.
+## Appending negative and positive class intervals together.
 
-intermatrix <- merge(as.data.frame(positive_class_labels), as.data.frame(negative_class_labels), all= TRUE) ## positive and negative classes ##
+positive_and_negative_class_intervals <- merge(as.data.frame(positive_class_labels), as.data.frame(negative_class_labels), all=TRUE) ## positive and negative classes
 
-
-## Exporting 'intermatrix1' and 'input_score' as bed files to merge.
-write.table(intermatrix, "./data/class_labels.bed", sep ='\t', quote = FALSE, row.names = FALSE)
+## Exporting 'positive_and_negative_class_intervals' and 'input_score' as bed files to merge.
+write.table(positive_and_negative_class_intervals, "./data/class_labels.bed", sep ='\t', quote = FALSE, row.names = FALSE)
 write.table(input_score, "./data/score.bed", sep ='\t', quote = FALSE, row.names = FALSE)
+
+## Processing for syntax and merging
+
+system("awk '{if (NR!=1) {print}}' ./data/class_labels.bed > ./data/class_labels_header_removed.bed")
+system("awk '{if (NR!=1) {print}}' ./data/score.bed > ./data/score_header_removed.bed")
+
+system("bedtools intersect -wa -wb -a ./data/score_header_removed.bed -b ./data/class_labels_header_removed.bed > ./data/score_labels.bed")
+
+## While merging the intervals for class labels (positive and negative) and RPKM normalized scores, there are occassions where one interval of the latter may
+## encompass several intervals from the former. This is reflected in the 'bedtools intersect' operation.
 
 ## Importing the merged file
 score_labels <- read.table("./data/score_labels.bed", sep = "\t", header = FALSE, stringsAsFactors=FALSE)
 
 ## Picking relevant columns
 final_data <- score_labels[,c(6:9,15)]
-colnames(final_data)=c("peaks_h3k27ac","peaks_h3k4me3","peaks_h3k4me2","peaks_h3k4me1","class")
-
-## The matrix is a sparse matrix, having many non-zero entries. Let us convert it into a sparse matrix object.
-library(Matrix)
-final_sparse_data <- final_data
-final_sparse_data$class <- as.numeric(as.factor(final_sparse_data$class))-1
-final_sparse_data$peaks_h3k27ac <- as.numeric(final_sparse_data$peaks_h3k27ac)
-final_sparse_data$peaks_h3k4me3 <- as.numeric(final_sparse_data$peaks_h3k4me3)
-final_sparse_data$peaks_h3k4me2 <- as.numeric(final_sparse_data$peaks_h3k4me2)
-final_sparse_data$peaks_h3k4me1 <- as.numeric(final_sparse_data$peaks_h3k4me1)
-
-final_sparse_data <- final_sparse_data[complete.cases(final_sparse_data), ]
+colnames(final_data)=c("reads_h3k27ac","reads_h3k4me3","reads_h3k4me2","reads_h3k4me1","class")
 
 ## Saving this data
-saveRDS(final_sparse_data,"./data/ep_data.rds")
+saveRDS(final_data,"./data/ep_data.rds")
 
 ## Sample data
-final_sparse_data_sample <- final_sparse_data[sample(nrow(final_sparse_data), 10000), ]
-saveRDS(final_sparse_data_sample,"./data/ep_data_sample.rds")
+final_data_sample <- final_data[sample(nrow(final_data), 10000), ]
+saveRDS(final_data_sample,"./data/ep_data_sample.rds")
 
 ## Converting data frame to matrix and then to sparse matrix.
 final_sparse_data_matrix <- data.matrix(final_sparse_data)
@@ -123,3 +119,7 @@ final_sparse_data_matrix <- Matrix(final_sparse_data_matrix, sparse=TRUE)
 ## Let's visualize.
 head(final_sparse_data_matrix)
 print(object.size(final_sparse_data_matrix),units="auto")
+
+
+## Saving relevant files
+save(positive_class_labels, tss_rev, p300_rev_finale, input_score, hg38_random, dhs_min, file = "relevant_files.RData")
--- Machine_Learning_Deep_Learning/train_data_preparation.R
+++ Machine_Learning_Deep_Learning/train_data_preparation.R
@@ -5,114 +5,147 @@
 # INSTEAD OF DOWNLOADING THE BED FILES AND USING THEM DIRECTLY, LETS CONVERT THEM TO BAM AND MODIFY THEIR INTERVALS  ###############
 # OR ######  USE THE FOLLOWING CHUNK ###############################################################################################
 ####################################################################################################################################
-# dhs_r <- download.file("ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM878nnn/GSM878621/suppl/GSM878621%5FUW%2EH1%2EChromatinAccessibility%2EDS19100%2Ebed%2Egz", "./delme") 
+
+# Downloading the relevant files #
+# DHS data #
+download.file("ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM878nnn/GSM878621/suppl/GSM878621%5FUW%2EH1%2EChromatinAccessibility%2EDS19100%2Ebed%2Egz", "./data/H1_Cell_Line/GSM878621_H1_DNase.bed.gz")
+system("gunzip ./data/H1_Cell_Line/GSM878621_H1_DNase.bed.gz")
+
+# P300 data #
+download.file("ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM831nnn/GSM831036/suppl/GSM831036%5F635EFAAXX%2E4%2EH1%5FP300%2Ealn%2Ebed%2Egz", "./data/H1_Cell_Line/GSM831036_H1_P300.bed.gz")
+system("gunzip ./data/H1_Cell_Line/GSM831036_H1_P300.bed.gz")
+
+
+# Sorting the data #
+system("sortBed -i ./data/H1_Cell_Line/GSM878621_H1_DNase.bed > ./data/H1_Cell_Line/GSM878621_H1_DNase_sorted.bed")
+system("sortBed -i ./data/H1_Cell_Line/GSM831036_H1_P300.bed > ./data/H1_Cell_Line/GSM831036_H1_P300_sorted.bed")
+
+# Working on DHS data #
 
 library(rtracklayer)
-dhs_try <- import.bed("./data/H1_Cell_Line/GSM878621_H1_DNase_sorted.bed")
-delme <- dhs_try[seqnames(dhs_try) == chromosomes]
-
-library(GenomicRanges)
-dhs_try_resize <- resize(delme, width = 2000)
-delme <- as.data.frame(dhs_try_resize)
-export.bed(dhs_try_resize, "delme.bed")
-
-system("bedtools sort -i delme.bed > delme_sorted.bed")
-system("mergeBed -i delme_sorted.bed > delme_sorted_merged.bed")
-
-####################################################################################################################################
-
-dhs <- read.csv("./data/H1_Cell_Line/GSM878621_H1_DNase_sorted.bed", sep = '\t', header = FALSE)
-p300 <- read.csv("./data/H1_Cell_Line/GSM831036_H1_P300_sorted.bed", sep = '\t', header = FALSE)
-
-## We see that the chromosome names in p300 data are just numbers. Let's add "chr" in the beginning for consistency. 
-p300$V1 <- paste0("chr",p300$V1)
-
-## Selecting useful columns: chrom, start, end.
-p300 <- p300[,c(1,2,3)]
-dhs <- dhs[,c(1,2,3)]
-
-## Sifting standard chromosomes.
-colnames(dhs) <- c("chrom","start","end")
-colnames(p300) <- c("chrom","start","end")
-
+dhs_rev <- import.bed("./data/H1_Cell_Line/GSM878621_H1_DNase_sorted.bed")
 chromosomes <- c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14",
                  "chr15", "chr16", "chr17", "chr18", "chr19", "chr20","chr21", "chr22", "chrX", "chrY")
-dhs <- as.data.frame(dhs[dhs$chrom %in% chromosomes, ])
-p300 <- as.data.frame(p300[p300$chrom %in% chromosomes, ])
+dhs_rev_chr <- dhs_rev[seqnames(dhs_rev) %in% chromosomes]
 
-## Saving files
+library(GenomicRanges)
+dhs_rev_chr_resize <- resize(dhs_rev_chr, width = 2000)
+dhs_final <- as.data.frame(dhs_rev_chr_resize)
+export.bed(dhs_final[,1:3], "dhs_final.bed")
 
-write.table(p300,"./data/H1_Cell_Line/h1_p300.bed", sep="\t", row.names=FALSE, quote = FALSE)
-write.table(dhs,"./data/H1_Cell_Line/h1_dhs.bed", sep="\t", row.names=FALSE, quote = FALSE)
+# for data integrity
+system("awk '$2>0 && $3>$2 {print $1 "\t" $2 "\t" $3}' dhs_final.bed > dhs_finale.bed")
+system("sortBed -i dhs_finale.bed > dhs_finale_sorted.bed")
 
+# Working on P300 data #
 
-## Saving as a final intersection of p300 and DHS sites
-system("intersectBed -a ./data/H1_Cell_Line/h1_p300_merged.bed -b ./data/H1_Cell_Line/h1_dhs_merged.bed > ./data/H1_Cell_Line/h1_p300_dhs_intersect.bed")
+p300_rev <- import.bed("./data/H1_Cell_Line/GSM831036_H1_P300_sorted.bed")
+BiocManager::install("diffloop")
+library(diffloop)
+p300_rev <- addchr(p300_rev) # Add prefix "chr" to the seqnames #
+p300_rev_chr <- p300_rev[seqnames(p300_rev) %in% chromosomes]
 
+p300_rev_chr_resize <- resize(p300_rev_chr, width = 2000)
+p300_rev_final <- as.data.frame(p300_rev_chr_resize)
+p300_rev_final <- unique(p300_rev_final[,c("seqnames","start","end")]) # removing redundant entries #
+export.bed(p300_rev_final, "p300_rev_final.bed")
 
-## Add class to the data: "enhancer"
+system("sortBed -i p300_rev_final.bed > p300_rev_final_sorted.bed")
+system("awk '{print $1 "\t" $2 "\t" $3}' p300_rev_final_sorted.bed > p300_rev_finale.bed")
 
-h1_p300_dhs_intersect <- read.table("./data/H1_Cell_Line/h1_p300_dhs_intersect.bed", sep = "\t")
-h1_p300_dhs_intersect$V4 <- "Enhancer"
-write.table(h1_p300_dhs_intersect,"./data/H1_Cell_Line/h1_p300_dhs_intersect_class.bed",sep="\t",row.names=FALSE, quote = FALSE)
+## Now that we have the p300 bindings genomewide, we shall filter them for being distant to the promoter/TSS regions. Note that we are 
+## solely interested in the ones that are distal to the TSS; distal as in non-proximal! 
+## GRanges objects are workabkle for set operations.
 
-#################################################################################################################
+p300_rev_finale <- import.bed("p300_rev_finale.bed")
 
-## Negative Class Labels ##
+## Let's pull the TSS sites
 
 tss_sites <- read.table("./data/H1_Cell_Line/TSS_Indices_Human_Genome.txt", sep = "\t", header = TRUE)
 tss_sites$Chromosome.scaffold.name <- paste0("chr", tss_sites$Chromosome.scaffold.name)
 tss_sites <- as.data.frame(tss_sites[tss_sites$Chromosome.scaffold.name  %in% chromosomes, ])
 tss_sites <- tss_sites[order(tss_sites$Chromosome.scaffold.name),]
+colnames(tss_sites) <- c("chrom", "start", "end")
+tss_rev <- GRanges(tss_sites)
 
-## Export TSS sites to create an overlap with DHS sites
+## We are going to shove off TSS sites that intersect with p300 binding sites. This will give us the distal p300 sites.
 
-write.table(tss_sites,"./data/H1_Cell_Line/tss_sites.bed", sep="\t", row.names=FALSE, quote = FALSE)
+p300_nonTSS <- setdiff(p300_rev_finale,tss_rev)
+export.bed(p300_nonTSS, "p300_nonTSS.bed")
+system("awk '{print $1 "\t" $2 "\t" $3}' p300_nonTSS.bed > p300_nonTSS_final.bed")
+
+## Finally, saving as a intersection of distal p300 and DHS sites
+
+system("intersectBed -a dhs_finale_sorted.bed -b p300_nonTSS_final.bed > h1_distalp300_dhs_intersect.bed")
 
 
-## Shifting intervals forward by 3000 bp
-tss_sites_test <- tss_sites
-tss_sites_test$Transcript.start..bp. <- tss_sites_test$Transcript.start..bp.+3000
-tss_sites_test$Transcript.end..bp. <- tss_sites_test$Transcript.end..bp.+3000
+## Add class to the data: "enhancer"
 
-## Sourcing chromosomal lengths in the human genome for generating random tracks
+h1_distalp300_dhs_intersect <- read.table("h1_distalp300_dhs_intersect.bed", sep = "\t")
+h1_distalp300_dhs_intersect$V4 <- "Enhancer"
+write.table(h1_distalp300_dhs_intersect,"h1_distalp300_dhs_intersect_class.bed",sep="\t",row.names=FALSE, quote = FALSE)
 
-hg19_chrom_sizes <- read.table(url("https://genome.ucsc.edu/goldenPath/help/hg19.chrom.sizes"), sep = "\t", header = FALSE, col.names = c("chrom", "size"))
-hg19_chrom_sizes <- as.data.frame(hg19_chrom_sizes[hg19_chrom_sizes$chrom %in% chromosomes, ])
+#################################################################################################################
+
+## Negative Class Labels ##
+
+# Here, we intend to find the intersection of TSS with DHS sites.
+dhs_min <- import.bed("dhs_finale_sorted.bed")
+tss_dhs <- intersect(tss_rev, dhs_min)
+
+
+## The other aspect for mapping negative class labels (non-enhancers) entails sourcing chromosomal lengths in the human genome for generating random tracks
+
+hg38_chrom_sizes <- read.table(url("https://genome.ucsc.edu/goldenPath/help/hg38.chrom.sizes"), sep = "\t", header = FALSE, col.names = c("chrom", "size"))
+hg38_chrom_sizes <- as.data.frame(hg38_chrom_sizes[hg38_chrom_sizes$chrom %in% chromosomes, ])
 
 ## Saving file for generating random tracks via 'bedtools random' function ##
-write.table(hg19_chrom_sizes,"./data/H1_Cell_Line/hg19.genome", sep="\t", row.names=FALSE, quote = FALSE)
+write.table(hg38_chrom_sizes,"./data/H1_Cell_Line/hg38.genome", sep="\t", row.names=FALSE, quote = FALSE)
+
+## Generating random tracks
+
+system("bedtools random -l 2000 -g ./data/H1_Cell_Line/hg38.genome > ./data/H1_Cell_Line/hg38_random_tracks.bed")
 
 ## Recalling
 
-hg19_random_tracks <- read.table("./data/H1_Cell_Line/hg19_random_tracks.bed", sep = "\t", header = FALSE)
-hg19_random_tracks <- hg19_random_tracks[,c(1,2,3)]
-hg19_random_tracks_ordered <- hg19_random_tracks[order(hg19_random_tracks[,1],hg19_random_tracks[,2]),]
+hg38_random_tracks <- read.table("./data/H1_Cell_Line/hg38_random_tracks.bed", sep = "\t", header = FALSE)
+hg38_random_tracks <- hg38_random_tracks[,c(1,2,3)]
+hg38_random_tracks_ordered <- hg38_random_tracks[order(hg38_random_tracks[,1],hg38_random_tracks[,2]),]
 
-write.table(hg19_random_tracks_ordered,"./data/H1_Cell_Line/hg19_random_tracks_sorted_required.bed", sep="\t", row.names=FALSE, quote = FALSE)
+write.table(hg38_random_tracks_ordered,"./data/H1_Cell_Line/hg38_random_tracks_sorted_required.bed", sep="\t", row.names=FALSE, quote = FALSE)
+colnames(hg38_random_tracks_ordered) <- c("chrom", "start", "end")
+hg38_random <- GRanges(hg38_random_tracks_ordered)
 
-## Choosing random sites distal to TSS
+## Choosing random sites distal to TSS and p300 bindings
 
 ## The strategy is to combine random tracks distal to the TSS or p300 binding sites. Now, let us create a 
 ## combination of TSS and p300 binding sites and then subtract the random sites from these, thus giving us
 ## the residuals.
 
-tss_final <- read.table("./data/H1_Cell_Line/tss_sites_header_removed.bed", sep = "\t", header = FALSE)
-p300_final <- read.table("./data/H1_Cell_Line/h1_p300_merged.bed", sep = "\t", header = FALSE)
 
 ## Combine the intervals and not 'merge' them
-tss_and_p300 <- rbind(tss_final,p300_final)
+p300_tss <- union(p300_rev_finale, tss_rev)
 
-## sort on the basis of chromosome names.
-tss_and_p300 <- tss_and_p300[order(tss_and_p300[,1]),]
+
+## Inferring random tracks distal to p300 bindings and TSS sites.
+random_nonp300TSS <- setdiff(hg38_random,p300_tss)
+
+
+## Merging the aforementioned random tracks and the TSS, DHS intersection.
+random_tss_dhs <- union(random_nonp300TSS, tss_dhs)
 
 ## Output file.
-write.table(tss_and_p300,"./data/H1_Cell_Line/tss_and_p300.bed", sep="\t", row.names=FALSE, quote = FALSE)
-
+write.table(random_tss_dhs,"./data/H1_Cell_Line/random_tss_dhs.bed", sep="\t", row.names=FALSE, quote = FALSE)
+system("awk '{if (NR!=1) {print}}' ./data/H1_Cell_Line/random_tss_dhs.bed > ./data/H1_Cell_Line/random_tss_dhs_header_removed.bed")
 
 ## Import resultant files from intersection.
-negative_class <- read.table("./data/H1_Cell_Line/negative_class.bed", sep = "\t", header = FALSE)
+negative_class <- read.table("./data/H1_Cell_Line/random_tss_dhs_header_removed.bed", sep = "\t", header = FALSE)
 negative_class$V4 <- "Non-Enhancer"
+negative_class$V5 <- c() # removing strand information
 write.table(negative_class,"./data/H1_Cell_Line/negative_class.bed", sep="\t", row.names=FALSE, quote = FALSE)
 
-positive_class <- read.table("./data/H1_Cell_Line/h1_p300_dhs_intersect_class_header_removed.bed", sep = "\t", header = FALSE)
+positive_class <- read.table("h1_distalp300_dhs_intersect_class.bed", sep = "\t", header = FALSE)
+
+## Saving auxilliary informaiton
+save(p300_rev, dhs_rev, file="aux.RData")
+
