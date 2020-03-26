3851530f6fb37d6927cc050225b50153603d0bef
Shaurya Jauhari <ishauryajauhari@gmail.com>
2020-03-26 20:28:54 +0800

    Rename file | modify script

--- Machine_Learning_Deep_Learning/terminalScripts/deepToolsmultiBAMSummaryPositiveClass.sh
+++
@@ -1,7 +0,0 @@
-multiBamSummary bins --bamfiles \
-/Users/soumyajauhari/Desktop/Machine_Learning/Machine_Learning_Deep_Learning/ENCFF832OFG.bam \
-/Users/soumyajauhari/Desktop/Machine_Learning/Machine_Learning_Deep_Learning/ENCFF923SKV.bam \
---binSize 2000 \
---labels EP300 DHS \
--out readCountsPositiveClass.npz \
---outRawCounts readCountsPositiveClass.tab
